d15cd3b45821fb7d8557e59155cb9329e8b3e609
ArchiPelado69 <83459179+ArchiPelado69@users.noreply.github.com>
2025-08-04 23:51:43 -0300

    Scene Setup & Object Clicked Detection

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+
+    private Camera _mainCamera;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if (!context.started) return;
+
+        var rayhit = Physics2D.GetRayIntersection(_mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue()));
+        if (!rayhit.collider) return;
+
+        Debug.Log(rayhit.collider.gameObject.name);
+    }
+}
